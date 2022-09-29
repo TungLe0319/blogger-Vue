3805e6aed504ad2e53638e2a9fe20845a0635ae1
Tung Le <tung.le0319@gmail.com>
2022-09-29 12:24:28 -0600

    tung

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-md-6">
         <div class="con-example-images">
+          
           <vs-images>
             <vs-image
               :key="index"
@@ -17,11 +18,41 @@
           </vs-images>
         </div>
       </div>
+      <div class="col-md-6 mt-5">
+ <vs-row vs-justify="center">
+    <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+      <vs-card actionable class="cardx">
+        <div slot="header">
+          <h3>
+           Blog Title
+          </h3>
+        </div>
+        <div slot="media" class="d-flex justify-content-center">
+          <img src="//thiscatdoesnotexist.com" alt="" class="forcedImg">
+        </div>
+        <div>
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+        </div>
+        <div slot="footer">
+          <vs-row vs-justify="flex-end">
+            <vs-button color="primary" type="gradient" >View</vs-button>
+            <vs-button color="danger" type="gradient">Delete</vs-button>
+          </vs-row>
+        </div>
+      </vs-card>
+    </vs-col>
+   
+  </vs-row>
+      </div>
+      <div class="col-md-12">
+        
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  
 export default {};
 </script>
 
@@ -29,4 +60,15 @@ export default {};
 .con-example-images {
   max-height: 500px overflow auto;
 }
+
+.forcedImg{
+  height: 300px;
+  width: 300px;
+  object-fit: cover;
+   //top/right/bottom/left  for margin;
+  margin:2px 2px 2px 2px;
+  border-radius: 4px;
+}
+
+
 </style>
---
+++ src/pages/testpage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="component"></div>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+
+  setup(props) {
+
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.text-shadow{
+  color: aliceblue;
+  text-shadow: 1px 1px black, 0px 0px 5px salmon;
+  font-weight: bold;
+  letter-spacing: 0.08rem
+  /* Second Color  in text-shadow is the blur */
+}
+.forcedImg{
+  height: 300px;
+  width: 300px;
+  object-fit: cover;
+}
+
+
+</style>
