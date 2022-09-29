880c31a212ecf5f0eab4881a09fe018bb1a5da28
daniel le <daniel97.le@gmail.com>
2022-09-29 13:54:41 -0600

    commit

--- src/services/BlogsService.js
+++ src/services/BlogsService.js
@@ -1,8 +1,11 @@
-class  BlogsService{
-  async getBlogs(){
-    const res = await ServerApi.get('/api/blogs')
-    AppState.blogs = 
+import { AppState } from "../AppState.js";
+import { Blog } from "../models/Blog.js";
+
+class BlogsService {
+  async getBlogs() {
+    const res = await ServerApi.get("/api/blogs");
+    AppState.blogs = res.data.map((b) => new Blog(res.data));
   }
 }
 
-export const blogsService = new BlogsService()
+export const blogsService = new BlogsService();
